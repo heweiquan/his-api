a59dc414cc9f42bff60aa4452793caabc4b7a2fb
heweiquan <hwq@lenovo.com>
2025-10-10 14:14:48 +0800

    4-2

---
+++ src/main/java/com/example/hisapi/HisApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HisApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hisapi/HisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
