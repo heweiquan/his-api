41c4983206b61727b3769a4a2d8c5579e603e147
heweiquan <hwq@lenovo.com>
2025-10-11 09:35:13 +0800

    4-9

--- src/main/java/com/example/hisapi/HisApiApplication.java
+++ src/main/java/com/example/hisapi/HisApiApplication.java
@@ -1,9 +1,13 @@
 package com.example.hisapi;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.*")
+@MapperScan("com.example.his.api.db.dao")
 public class HisApiApplication {
 
     public static void main(String[] args) {
